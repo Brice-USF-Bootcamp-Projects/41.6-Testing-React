dbd76f72a6961417cfe575734fe7007d57b7f54a
Brice Nelson <bnelsonemail@icloud.com>
2025-01-20 15:10:56 -0500

    revisions to carousel and caraousel test.  added missing dependencies to package json.

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,51 +1,62 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 import "./Carousel.css";
 import Card from "./Card";
 
-
-/** Carousel: displays images and arrows to navigate through them
- * 
- * Props:
- * - photos: array of {src, caption} objects
- * - title: string describing the collection of images
- * 
- * State:
- * - currCardIdx: integer for current card index
- * 
- * App --> Carousel --> Card
- */
- function Carousel({ photos, title }) {
+function Carousel({ photos, title }) {
   const [currCardIdx, setCurrCardIdx] = useState(0);
+
+  if (!photos.length) {
+    return (
+      <div className="Carousel">
+        <h1>{title}</h1>
+        <p>No images to display.</p>
+      </div>
+    );
+  }
 
   const currCard = photos[currCardIdx];
   const total = photos.length;
 
-  //Increments currCardIdx state by 1
   function goForward() {
-    setCurrCardIdx(currCardIdx + 1);
+    setCurrCardIdx((idx) => (idx + 1) % total);
+  }
+
+  function goBackward() {
+    setCurrCardIdx((idx) => (idx - 1 + total) % total);
   }
 
   return (
     <div className="Carousel">
       <h1>{title}</h1>
       <div className="Carousel-main">
-        <i
-          className="bi bi-arrow-left-circle"
-          onClick={goForward}
-        />
+        <i className="bi bi-arrow-left-circle" onClick={goBackward} />
         <Card
           caption={currCard.caption}
           src={currCard.src}
           currNum={currCardIdx + 1}
           totalNum={total}
         />
-        <i
-          className="bi bi-arrow-right-circle"
-          onClick={goForward}
-        />
+        <i className="bi bi-arrow-right-circle" onClick={goForward} />
       </div>
     </div>
   );
 }
 
+Carousel.propTypes = {
+  photos: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string.isRequired,
+      caption: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+Carousel.defaultProps = {
+  photos: [],
+  title: "Image Carousel",
+};
+
 export default Carousel;
+
